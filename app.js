ee1df23a0b8e56ac440f09961a63a4f41196cf70
Xavier McLallen <xmclallen@gmail.com>
2021-07-06 16:48:34 -0700

    Full CI/CD pipeline, including manual review, test commit.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(`It's alive!`));
+app.get('/', (req, res) => res.send(`It's alive! I set up this app with AWS CodePipeline, CodeBuild, and Elastic Beanstalk, sourced from a github repository.`));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
